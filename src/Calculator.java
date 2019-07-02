116950e048e33d58a3f3af2b40d6cbdb69a55d63
Hephaest <philliphily@gmail.com>
2019-07-02 10:50:30 -0500

    Second Update.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,6 @@
 /**
  * @author Hephaest
- * @since  2018/04/19
+ * @since  2019/07/02
  * JDK 1.6
  */
 import java.awt.BorderLayout;
@@ -13,18 +13,18 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 /**
- * @author phili2
  * Calculator类用来创造GUI
  */
 public class Calculator extends JFrame
 {
-    //新建文本框
+	//新建文本框
 	JTextField text = new JTextField();
 	// set up row 2
 	JPanel row2 = new JPanel();
 	//创建按钮们
-	String[][] buttons= {{"7","8","9","DEL","AC"},{"4","5","6","×","÷"},{"1","2","3","+","-"},{"0","(",")","Replay","="}};
+	String[][] buttons = {{"7","8","9","DEL","AC"},{"4","5","6","×","÷"},{"1","2","3","+","-"},{"0","(",")","Replay","="}};
 	JButton[][]button = new JButton[4][5];
+
 	/**
 	 * 这个计算机的界面我模拟的是卡西欧fx-82ES PLUS A
 	 * 但是仅有其中的部分功能
@@ -43,47 +43,48 @@ public class Calculator extends JFrame
 		//声明每一个按钮代表的意义
 		add(row2, BorderLayout.CENTER);
 		GridLayout layout2 = new GridLayout(4,5,5,5);
-    	row2.setLayout(layout2);
-    	for(int i=0;i<buttons.length;i++)
+		row2.setLayout(layout2);
+		for(int i = 0; i < buttons.length; i++)
 		{
-			for(int j=0;j<buttons[0].length;j++)
+			for(int j = 0; j < buttons[0].length; j++)
 			{
-				button[i][j]=new JButton(buttons[i][j]);
+				button[i][j] = new JButton(buttons[i][j]);
 				row2.add(button[i][j]);
 			}
 		}
-    	add(row2);
-        setResizable(false);
-    	setVisible(true);
+		add(row2);
+		setResizable(false);
+		setVisible(true);
 	}
-	
-	private static void setLookAndFeel() 
+
+	private static void setLookAndFeel()
 	{
 		//这条使跨操作系统也能看到计算机的GUI
-        try {
-            UIManager.setLookAndFeel(
-                "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel"
-            );
-        } catch (Exception exc) {
-            // ignore error
-        }
-    }
-	
-	public static void main(String[] args) 
+		try {
+			UIManager.setLookAndFeel(
+					"com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel"
+			);
+		} catch (Exception exc) {
+			// ignore error
+		}
+	}
+
+	public static void main(String[] args)
 	{
 		Calculator.setLookAndFeel();
 		Calculator cl = new Calculator();
 		cl.listener();
 	}
+
 	/**
 	 * 事件监听器，一旦按下按钮就要根据操作历史进行相应的反应
 	 */
 	public void listener()
 	{
-		Listener l=new Listener(this);
-		for(int i=0;i<buttons.length;i++)
+		Listener l = new Listener(this);
+		for(int i = 0; i < buttons.length; i++)
 		{
-			for(int j=0;j<buttons[0].length;j++)
+			for(int j = 0; j < buttons[0].length; j++)
 			{
 				button[i][j].addActionListener(l);
 			}
--- src/Listener.java
+++ src/Listener.java
@@ -1,19 +1,15 @@
 /**
  * @author Hephaest
- * @since  2018/04/19
+ * @since  2019/07/02
  * JDK 1.6
  */
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import javax.swing.JButton;
-import javax.swing.JTextField;
+
 /**
  * Listener 类
- * @author phili2
  * 用来将按钮输入的结果通过链表的方式一个字一个字存储在字符串里，然后调用另一类计算整个字符串，返回一个值
  */
 public class Listener implements ActionListener
@@ -21,124 +17,118 @@ public class Listener implements ActionListener
 	private Calculator cl;
 	private ArrayList<String> list=new ArrayList<String>();
 	private ArrayList<String> his=new ArrayList<String>();//这个链表用来添加每一次得到的最终的结果
-	private ArrayList<String> arr = new ArrayList<String>();
-	private String[] arrayStr = new String[] {};
-	private String out="";
-	private String output="";
-    
+	private ArrayList<String> arr = new ArrayList<String>();//把his里的一整串字符分割成单个字符，再连接
+	private String[] arrayStr = new String[] {};//储存单次的历史记录
+	private String out = "";
+	private String output = "";
+
 	public Listener(Calculator cl)
-    {
-    	this.cl=cl;
-    }
-    
+	{
+		this.cl = cl;
+	}
+
 	public void actionPerformed(ActionEvent event)
 	{
 		JButton button = (JButton) event.getSource();
+		/**
+		 * 如果点“=”，计算整个表达式的结果，如果是错误表达式，在文本框输入“Input Error!”
+		 */
 		if(button.getText().equals("="))
 		{
 			try
-			{	
-				function f = new function();
-				double result=f.compute(output);
-				cl.text.setText(Double.toString(result));
-			}catch(Exception e)
 			{
+				Function f = new Function();
+				double result = f.compute(out);
+				cl.text.setText(Double.toString(result));
+			} catch(Exception e) {
 				cl.text.setText("Input Error!");
 			}
-			
-		}
-		else if(button.getText().equals("×"))
-		{
+		} else if(button.getText().equals("×")) {
+			/**
+			 * 如果点击"×"，先把它转换为"*"
+			 */
 			if(list.isEmpty())
 			{
 				arr.add("*");
-				output+="*";
-				out=output;
+				output += "*";
+				out = output;
 				cl.text.setText(output);
-			}
-			else
-			{
+			} else {
 				list.add("*");
-				output+="*";
-				out=output;
+				output += "*";
+				out = output;
 				cl.text.setText(output);
 			}
-		}
-		else if(button.getText().equals("÷"))
-		{
+		} else if(button.getText().equals("÷")) {
+			/**
+			 * 如果点击"÷"，把它转换为"/"
+			 */
 			if(list.isEmpty())
 			{
 				arr.add("/");
-				output+="/";
-				out=output;
+				output += "/";
+				out = output;
 				cl.text.setText(output);
-			}
-			else
-			{
+			} else {
 				list.add("/");
-				output+="/";
-				out=output;
+				output += "/";
+				out = output;
 				cl.text.setText(output);
 			}
-		}
-		else if(button.getText().equals("DEL"))
-		{
+		} else if(button.getText().equals("DEL")) {
+			/**
+			 * 如果点击"DEL"，删除表达式里最后一个字符，每点一次删一个
+			 */
 			if(list.isEmpty())
 			{
 				arr.remove(arr.size()-1);
-		         output="";
-	             for(int i=0;i<arr.size();i++)
-	 			 {
-	 				output+=arr.get(i);
-	 			 }
-	 			out=output;
-	 			cl.text.setText(output);
+				output = "";
+				for(int i = 0; i < arr.size(); i++) output += arr.get(i);
+				out = output;
+				cl.text.setText(output);
+			} else {
+				list.remove(list.size()-1);
+				String output = "";
+				for(int i = 0; i < list.size(); i++) output+=list.get(i);
+				out = output;
+				cl.text.setText(output);
 			}
-			else
-			{
-	             list.remove(list.size()-1);
-	             String output="";
-	             for(int i=0;i<list.size();i++)
-	 			 {
-	 				output+=list.get(i);
-	 			 }
-	 			out=output;
-	 			cl.text.setText(output);
-			}
-		}
-		else if(button.getText().equals("AC"))
-		{
+		} else if(button.getText().equals("AC")) {
+			/**
+			 * 如果点击"AC"，删除list链表，再删除之前先把表达式保留到his的链表里
+			 */
 			his.add(out);
 			list.clear();
 			output="";
- 			cl.text.setText(output);
-		}
-		else if(button.getText().equals("Replay"))
-		{
+			cl.text.setText(output);
+		} else if(button.getText().equals("Replay")) {
+			/**
+			 * 如果点击"Replay"，在文本框里显示上一条表达式
+			 */
 			output=his.get(his.size()-1);
 			cl.text.setText(output);
 			arr.clear();
+			//把上一条表达式分割成单个字符的字符数组
 			char[] a=output.toCharArray();
 			for(int i=0;i<a.length;i++)
 			{
 				arr.add(String.valueOf(a[i]));
 			}
 			his.remove(his.size()-1);
-		}
-		else
-		{
+		} else {
+			/**
+			 * 其余按钮可以直接加入表达式
+			 */
 			if(list.isEmpty())
 			{
 				arr.add(button.getText());
 				output+=button.getText();
-//				out=output;
+				out=output;
 				cl.text.setText(output);
-			}
-			else
-			{
+			} else {
 				list.add(button.getText());
 				output+=button.getText();
-//				out=output;
+				out=output;
 				cl.text.setText(output);
 			}
 		}
